527922f86d3a0cd0776ff0ed5363e836164fd786
sadmaningithub <sadmanahmed261@gmail.com>
2024-12-09 17:46:50 +0600

    user info succesfully received from the register form

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,6 +1,19 @@
 
 
 const Registration = () => {
+
+    const handleRegistration = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const profilePicture = form.profilePicture.value;
+        const role = form.role.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const user = {name, profilePicture, role, email, password}
+        console.log(user);
+    }
+
     return (
         <div>
             <div className="flex flex-col justify-center items-center min-h-screen">
@@ -15,25 +28,25 @@ const Registration = () => {
 
                     <div className="card bg-base-100 w-full shadow-2xl ">
 
-                        <form className="card-body">
+                        <form onSubmit={handleRegistration} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="name" className="input input-bordered" required />
+                                <input type="text" placeholder="name" name='name' className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Profile Picture Url</span>
                                 </label>
-                                <input type="url" placeholder="url" className="input input-bordered" required />
+                                <input type="url" placeholder="url" name="profilePicture" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Role</span>
                                 </label>
-                                <select className="select select-bordered w-full">
-                                    <option disabled selected>Select Any</option>
+                                <select name="role" className="select select-bordered w-full">
+                                    <option disabled defaultValue={'Worker'}></option>
                                     <option>Worker</option>
                                     <option>TaskCreator</option>
                                 </select>
@@ -42,19 +55,19 @@ const Registration = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" placeholder="password" name="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Register</button>
+                                <input className="btn" type="submit" value="Register" />
                             </div>
                         </form>
                     </div>
